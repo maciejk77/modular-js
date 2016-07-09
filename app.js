469e311c2a57ebeba1e886dc41e1870cf6ed5079
Maciej Kurek <maciejk77@gmail.com>
2016-07-09 12:21:31 +0100

    Revealing JS pattern, final update

--- app.js
+++ app.js
@@ -13,24 +13,29 @@ var people = (function() {
   $button.on('click', addPerson);
   $ul.delegate('i.del', 'click', deletePerson);
 
-  render();
+  _render();
 
-  function render() {
+  function _render() {
     $ul.html(Mustache.render(template, {people: people}));
   };
 
-  function addPerson() {
-    people.push($input.val());
-    render();
+  function addPerson(value) {
+    var name = (typeof value === "string") ? value : $input.val();
+    people.push(name);
+    _render();
     $input.val('');
   }
 
   function deletePerson(event) {
-    var $remove = $(event.target).closest('li');
-    var i = $ul.find('li').index($remove);
-
+    var i;
+    if(typeof event === "number") {
+      i = event;
+    } else {   
+      var $remove = $(event.target).closest('li');
+      i = $ul.find('li').index($remove);
+    }
     people.splice(i, 1);
-    render();
+    _render();
   }
 
   return {
