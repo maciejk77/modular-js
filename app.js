bdf34dbaee1d0057dfc0b7c4ecd93350d58d1602
Maciej Kurek <maciejk77@gmail.com>
2016-07-09 12:00:55 +0100

    Revealing module patern

--- app.js
+++ app.js
@@ -1,50 +1,44 @@
 
-(function() {
+var people = (function() {
+  var people = ['Maciek', 'Pat', 'Tom'];
 
-  var people = {
-    people: ['Maciek', 'Pat', 'Tom'],
-    init: function() {
-      this.cacheDom();
-      this.bindEvents();
-      this.render();
-    },
-    cacheDom: function() {
-      this.$el = $('#peopleModule');
-      this.$button = this.$el.find('button');
-      this.$input = this.$el.find('input');
-      this.$ul = this.$el.find('ul');
-      this.$template = this.$el.find('#people-template').html();
-    },
-    bindEvents: function() {
-      this.$button.on('click', this.addPerson.bind(this));
-      this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
-    },
-    render: function() {
-      var data = {
-        people: this.people,
-      };
-      this.$ul.html(Mustache.render(this.$template, data));
-    },
-    addPerson: function() {
-      this.people.push(this.$input.val());
-      this.render();
-      this.$input.val('');
-    },
-    deletePerson: function(event) {
-      var $remove = $(event.target).closest('li');
-      var i = this.$ul.find('li').index($remove);
+  // cache DOM
+  var $el = $('#peopleModule');
+  var $button = $el.find('button');
+  var $input = $el.find('input');
+  var $ul = $el.find('ul');
+  var template = $el.find('#people-template').html();
 
-      this.people.splice(i, 1);
-      this.render();
-    }
+  // bind events
+  $button.on('click', addPerson);
+  $ul.delegate('i.del', 'click', deletePerson);
+
+  render();
+
+  function render() {
+    $ul.html(Mustache.render(template, {people: people}));
   };
-  
-  people.init();
+
+  function addPerson() {
+    people.push($input.val());
+    render();
+    $input.val('');
+  }
+
+  function deletePerson(event) {
+    var $remove = $(event.target).closest('li');
+    var i = $ul.find('li').index($remove);
+
+    people.splice(i, 1);
+    render();
+  }
+
+  return {
+    addPerson: addPerson,
+    deletePerson: deletePerson 
+  };
 
 })();
-
-
-
 
 
 // SPAGHETTI CODE below 
