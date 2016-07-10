45576c012136e2b4b536da48e2b08d2254e5aa67
Maciej Kurek <maciejk77@gmail.com>
2016-07-10 13:51:47 +0100

    Instantiation with name provided

--- prototypal-inheritance.js
+++ prototypal-inheritance.js
@@ -2,6 +2,11 @@
 
 var human = {
   species: "human",
+  create: function(name) {
+    var instance = Object.create(this);
+    instance.name = name;
+    return instance;
+  },
   saySpecies: function() {
     console.log(this.species)
   },
@@ -10,15 +15,18 @@ var human = {
   }
 };
 
-var musician = Object.create(human);
-musician.playInstrument = function() {
-  console.log("He plays..." + this.instrument);
-};
+var maciek = human.create("Maciek");
+var marek = human.create("Marek");
+
+// var musician = Object.create(human);
+// musician.playInstrument = function() {
+//   console.log("He plays..." + this.instrument);
+// };
 
 var maciek = Object.create(musician);
-maciek.name = "Maciek";
+// maciek.name = "Maciek";
 maciek.playInstrument = "Drums";
 
 var marek = Object.create(musician);
-marek.name = "Marek";
+// marek.name = "Marek";
 marek.playInstrument = "Guitar";
